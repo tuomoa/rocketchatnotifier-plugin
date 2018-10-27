7f065e22cc68095a26fcb428e694ec15ca23e28f
Reinhardt, Martin <contact@martinreinhardt-online.de>
2018-10-27 09:07:34 +0200

    fix(Tests): Make ITs work again

--- src/test/java/jenkins/plugins/rocketchatnotifier/rocket/RocketChatClientCallBuilderIT.java
+++ src/test/java/jenkins/plugins/rocketchatnotifier/rocket/RocketChatClientCallBuilderIT.java
@@ -2,12 +2,11 @@ package jenkins.plugins.rocketchatnotifier.rocket;
 
 import hudson.ProxyConfiguration;
 import jenkins.model.Jenkins;
+import jenkins.plugins.rocketchatnotifier.rocket.errorhandling.RocketClientException;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.mockito.Mock;
-
-import java.io.IOException;
 
 public class RocketChatClientCallBuilderIT {
 
@@ -17,7 +16,7 @@ public class RocketChatClientCallBuilderIT {
   public JenkinsRule j = new JenkinsRule();
 
 
-  @Test(expected = IOException.class)
+  @Test(expected = RocketClientException.class)
   public void shouldEscapeSpecialCharacters() throws Exception {
     // given
     Jenkins.getInstance().proxy = null;
@@ -26,7 +25,7 @@ public class RocketChatClientCallBuilderIT {
     rocketCallBuilder.buildCall(RocketChatRestApiV1.ChannelsList);
     // then error
   }
-  @Test(expected = IOException.class)
+  @Test(expected = RocketClientException.class)
   public void shouldWorkWithProxy() throws Exception {
     // given
     Jenkins.getInstance().proxy = new ProxyConfiguration("sample1", 1234);
