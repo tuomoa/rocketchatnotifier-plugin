4108174cbeb7a4f95a095c224168e485909b3f0e
Martin Reinhardt <contact@martinreinhardt-online.de>
2019-01-26 08:57:14 +0100

    feat(Authentication): Adding support for personal token login

--- src/main/java/jenkins/plugins/rocketchatnotifier/rocket/RocketChatBasicCallAuthentication.java
+++ src/main/java/jenkins/plugins/rocketchatnotifier/rocket/RocketChatBasicCallAuthentication.java
@@ -28,8 +28,7 @@ public class RocketChatBasicCallAuthentication implements RocketChatCallAuthenti
     }
     if (!serverUrl.endsWith("api/")) {
       this.serverUrl = serverUrl + "api/";
-    }
-    else {
+    } else {
       this.serverUrl = serverUrl;
     }
     this.user = user;
@@ -45,25 +44,35 @@ public class RocketChatBasicCallAuthentication implements RocketChatCallAuthenti
   public void doAuthentication() throws RocketClientException {
     HttpResponse<JsonNode> loginResult;
     String apiURL = serverUrl + "v1/login";
-
+    String userInfoUrl = serverUrl + "v1/me";
     try {
       loginResult = Unirest.post(apiURL).field("user", user).field("password", password).asJson();
-    }
-    catch (UnirestException e) {
+    } catch (UnirestException e) {
       throw new RocketClientException("Please check if the server API " + apiURL + " is correct: " + e.getMessage(), e);
     }
 
     if (loginResult.getStatus() == 401) {
-      throw new RocketClientException("The username and password provided are incorrect.");
+      // try via token
+      try {
+        loginResult = Unirest.get(userInfoUrl).header("X-User-Id", user).header("X-Auth-Token", password).asJson();
+        if (loginResult.getStatus() == 401) {
+          throw new RocketClientException("The username and password provided are incorrect.");
+        }
+        this.userId = user;
+        this.authToken = password;
+      } catch (UnirestException e) {
+        // TODO more logging
+        throw new RocketClientException("Please check if the server API " + apiURL + " is correct: " + e.getMessage(), e);
+      }
+    } else {
+      JSONObject data = loginResult.getBody().getObject().getJSONObject("data");
+      this.userId = data.getString("userId");
+      this.authToken = data.getString("authToken");
     }
 
     if (loginResult.getStatus() != 200) {
       throw new RocketClientException("The login failed with a result of: " + loginResult.getStatus());
     }
-
-    JSONObject data = loginResult.getBody().getObject().getJSONObject("data");
-    this.authToken = data.getString("authToken");
-    this.userId = data.getString("userId");
   }
 
   @Override
