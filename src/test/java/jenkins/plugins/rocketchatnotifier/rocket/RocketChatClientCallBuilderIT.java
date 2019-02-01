2194363fac4d954a2695b1a5f215bca8f8c8791e
Martin Reinhardt <contact@martinreinhardt-online.de>
2019-02-01 15:48:56 +0100

    tests(CLientCallBuilderIT): use mocks instead of impl

--- src/test/java/jenkins/plugins/rocketchatnotifier/rocket/RocketChatClientCallBuilderIT.java
+++ src/test/java/jenkins/plugins/rocketchatnotifier/rocket/RocketChatClientCallBuilderIT.java
@@ -3,23 +3,32 @@ package jenkins.plugins.rocketchatnotifier.rocket;
 import hudson.ProxyConfiguration;
 import jenkins.model.Jenkins;
 import jenkins.plugins.rocketchatnotifier.rocket.errorhandling.RocketClientException;
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ Jenkins.class, ProxyConfiguration.class })
 public class RocketChatClientCallBuilderIT {
 
-  @Mock
-  private Jenkins jenkins;
-  @Rule
-  public JenkinsRule j = new JenkinsRule();
+
+  @Before
+  public void setup() {
+    PowerMockito.mockStatic(Jenkins.class);
+    PowerMockito.mockStatic(ProxyConfiguration.class);
+  }
+
 
 
   @Test(expected = RocketClientException.class)
   public void shouldEscapeSpecialCharacters() throws Exception {
     // given
-    Jenkins.getInstance().proxy = null;
     RocketChatClientCallBuilder rocketCallBuilder = new RocketChatClientCallBuilder("http://localhost", false, "]\",", "]\",");
     // when
     rocketCallBuilder.buildCall(RocketChatRestApiV1.ChannelsList);
@@ -28,7 +37,10 @@ public class RocketChatClientCallBuilderIT {
   @Test(expected = RocketClientException.class)
   public void shouldWorkWithProxy() throws Exception {
     // given
-    Jenkins.getInstance().proxy = new ProxyConfiguration("sample1", 1234);
+    Jenkins jenkinsMock = mock(Jenkins.class);
+    ProxyConfiguration proxyConf = mock(ProxyConfiguration.class);
+    when(Jenkins.getInstance()).thenReturn(jenkinsMock);
+    jenkinsMock.proxy = proxyConf;
     RocketChatClientCallBuilder rocketCallBuilder = new RocketChatClientCallBuilder("http://localhost", false, "]\",", "]\",");
     // when
     rocketCallBuilder.buildCall(RocketChatRestApiV1.ChannelsList);
