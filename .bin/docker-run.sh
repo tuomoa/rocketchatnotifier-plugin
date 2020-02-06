689be44491e2823090a6bf4ca09a947b97c3e8f5
Martin Reinhardt <contact@martinreinhardt-online.de>
2020-02-06 17:35:26 +0100

    chore(Build): Move scripts to same repo

--- .bin/docker-run.sh
+++ .bin/docker-run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source ./docker-env.sh
+source ./.bin/docker-env.sh
 
 # Starts all applications. Run 'mvn docker:build' in platform and webapp before this script to create docker images.
 
