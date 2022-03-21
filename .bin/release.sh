36dc090d4308aecd2f6259d02d2c04f9083e5587
Martin Reinhardt <contact@martinreinhardt-online.de>
2022-03-21 10:32:04 +0100

    chore(Build): Update Maven Gitflow plugin

--- .bin/release.sh
+++ .bin/release.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 git checkout develop
 git branch -D master
-mvn gitflow:release-start && \
+./mvnw gitflow:release-start && \
  conventional-changelog -p angular -i CHANGELOG.md -s -r 0 && \
  git add CHANGELOG.md && \
  git commit -m "Changelog" && \
- mvn gitflow:release-finish -DnoDeploy=true && \
+ ./mvnw gitflow:release-finish -DnoDeploy=true && \
  git push origin master && git push --tags && \
  git checkout develop
